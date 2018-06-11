4746caf3e1a9f50598ac53a013206d9c7c84bbb0
Szymon Targosz <szymon7.t@gmail.com>
2018-06-11 19:47:39 +0200

    Date and amount format added

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -3,12 +3,13 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expensesWithFilters';
 import computeSum from '../selectors/expensesTotal';
 import { Link } from 'react-router-dom';
+import numeral from 'numeral';
 
 export const Summary = ({ expensesSum, expensesCount }) => {
     const matchingWord = expensesCount === 1 ? 'expense' : 'expenses';
     return (
         <div>
-            <h1>Viewing <span>{expensesCount}</span> {matchingWord} totalling <span>{expensesSum}</span></h1>
+            <h1>Viewing <span>{expensesCount}</span> {matchingWord} totalling <span>{numeral(expensesSum / 100).format('$0,0.00')}</span></h1>
             <Link className='button' to='/create'>Add Expense</Link>
         </div>
     );
