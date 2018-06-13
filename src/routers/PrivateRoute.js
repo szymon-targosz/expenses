4f84cdf4d6902d8f5a9c369e40ff46d1167b70ac
Szymon Targosz <szymon7.t@gmail.com>
2018-06-13 22:18:45 +0200

    empty commit

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -3,7 +3,6 @@ import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
 import Header from '../components/Header';
 
-
 export const PrivateRoute = ({ isAuthenticated, component: Component, ...rest }) => (
     <Route {...rest} component={(props) => (
         isAuthenticated ? (
