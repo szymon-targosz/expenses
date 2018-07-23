5e15eee9ba4935c5c4bd0af168251551c34e5373
Szymon Targosz <szymon7.t@gmail.com>
2018-07-23 18:18:08 +0200

    Style: Filters

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -16,9 +16,9 @@ export class Filters extends React.Component {
     onSelectChange = (e) => {
         const val = e.target.value;
         if (val === 'date') {
-          this.props.sortByDate();  
+            this.props.sortByDate();
         } else if (val === 'amount') {
-          this.props.sortByAmount();  
+            this.props.sortByAmount();
         };
     };
 
@@ -33,32 +33,42 @@ export class Filters extends React.Component {
 
     render() {
         return (
-            <div>
-                <input 
-                    type='text'
-                    placeholder='Search'
-                    value={this.props.filters.text}
-                    onChange={this.onTextChange}    
-                />
-                <select 
-                    value={this.props.filters.sortBy}
-                    onChange={this.onSelectChange}
-                >
-                    <option value='date'>Date</option>
-                    <option value='amount'>Amount</option>
-                </select>
-                <DateRangePicker 
-                    startDate={this.props.filters.startDate}
-                    startDateId='start'
-                    endDateId='end'
-                    endDate={this.props.filters.endDate}
-                    focusedInput={this.state.calendarFocused}
-                    onFocusChange={this.onFocusChange}
-                    onDatesChange={this.onDatesChange}
-                    numberOfMonths={1}
-                    isOutsideRange={() => false}
-                    showClearDates={true}
-                />
+            <div className='content-container'>
+                <div className="input-group">
+                    <div className="input-group__item">
+                        <input
+                            className='text-input'
+                            type='text'
+                            placeholder='Search'
+                            value={this.props.filters.text}
+                            onChange={this.onTextChange}
+                        />
+                    </div>
+                    <div className="input-group__item">
+                        <select
+                            className='select'
+                            value={this.props.filters.sortBy}
+                            onChange={this.onSelectChange}
+                        >
+                            <option value='date'>Date</option>
+                            <option value='amount'>Amount</option>
+                        </select>
+                    </div>
+                    <div className="input-group__item">
+                        <DateRangePicker
+                            startDate={this.props.filters.startDate}
+                            startDateId='start'
+                            endDateId='end'
+                            endDate={this.props.filters.endDate}
+                            focusedInput={this.state.calendarFocused}
+                            onFocusChange={this.onFocusChange}
+                            onDatesChange={this.onDatesChange}
+                            numberOfMonths={1}
+                            isOutsideRange={() => false}
+                            showClearDates={true}
+                        />
+                    </div>
+                </div>
             </div>
         );
     }
--- src/components/List.js
+++ src/components/List.js
@@ -4,12 +4,17 @@ import { connect } from 'react-redux';
 import selectExpenses from '../selectors/expensesWithFilters';
 
 export const List = (props) => (
-    <div>
+    <div className='content-container'>
+        <div className="list-header">
+            <div className="show-for-mobile">Expenses</div>
+            <div className="show-for-desktop">Expenses</div>
+            <div className="show-for-desktop">Amount</div>
+        </div>
         {props.expenses.length === 0 ? (
             <div>no expenses</div>
         ) : (
-            props.expenses.map(expense => <ListItem key={expense.id} {...expense} />)
-        )}
+                props.expenses.map(expense => <ListItem key={expense.id} {...expense} />)
+            )}
     </div>
 );
 
