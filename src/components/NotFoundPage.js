c1a7538e1513e934e249c1248532cfca47c3c81c
Szymon Targosz <szymon7.t@gmail.com>
2018-07-26 18:01:47 +0100

    Style: NotFoundPage, hover effects; updated snapshots

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -2,8 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NotFoundPage = () => (
-    <div>
-        404! <Link to='/'>Go home</Link>
+    <div className='not-found'>
+        <div>
+            PAGE <span>NOT</span> FOUND
+        </div>
+        <div className='not-found__error'>
+            404
+        </div>
+        <Link className='button' to='/'>Go home</Link>
     </div>
 );
 
